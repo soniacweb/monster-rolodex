a01ddd4fc9b41973009703b9c4b904523ab06176
SoniaCWeb <sonia.choudhury@hotmail.co.uk>
2020-09-21 21:47:39 +0100

    Updates

---
+++ precache-manifest.b8830ab5fde141dcf262ccfcccceb636.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359b2cf9896e1334e362686c76d3f615",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "a86d9065f7704a917645",
+    "url": "/monster-rolodex/static/css/main.d0206fa0.chunk.css"
+  },
+  {
+    "revision": "984b0ae2615a0987e9c6",
+    "url": "/monster-rolodex/static/js/2.d0b5ee06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.d0b5ee06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a86d9065f7704a917645",
+    "url": "/monster-rolodex/static/js/main.59f5b81f.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
